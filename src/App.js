a9d9fc41be13138bd79f0abb2c81a8ba26d95875
Sebastian Rigeiro <seba_rigeiro@yahoo.com.ar>
2021-09-04 18:28:45 -0300

    Se agrega ItemCount

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar.js'
+import ItemCount from './components/ItemCount'
 import ItemListContainer from './components/ItemListContainer.js';
 
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <header>
+        <NavBar />
+      </header>
+
+      <section>  
       <ItemListContainer greeting="El listado de productos se encuentra en construccion"/>
+      <ItemCount />
+      </section>
+    
     </div>
+
   );
 }
 
---
+++ src/components/ItemCount.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+function ItemCount () {
+    const stock = 20;
+    const [ count, setCount ] = useState (0)
+    const addItem = () => {
+        if (count < stock) {
+            setCount (count + 1)
+        }
+    }
+    const removeItem = () => {
+        if (count > 0) {
+            setCount (count - 1)
+        }
+    }
+    
+    return (
+        <div className=" p-3 text-center">
+           {/*  <img src="" className="" /> */}
+            <div className="card-body">
+                <h5 className="card-subtitle">Toyota</h5>
+                <div className="d-inline-flex p-2 bd-highlight">                
+                    <button type="button" className="btn btn-secondary btn-sm" onClick={addItem}>+</button>
+                    <p className="card-text m-2">{count}</p>
+                    <button type="button" className="btn btn-secondary btn-sm "onClick={removeItem}>-</button>
+                </div>
+                <p>stock: {stock}</p>
+            </div>
+        </div>
+    )
+}
+
+export default ItemCount;
